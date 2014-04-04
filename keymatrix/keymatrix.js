4d7b66f1ae83ff967cd1b4598328f10d715b1ce9
RGBKey <rgbkey@gmail.com>
2014-04-04 19:14:58 -0400

    Whoops.

--- keymatrix/keymatrix.js
+++ keymatrix/keymatrix.js
@@ -1,4 +1,4 @@
 //Create the buttons
 for (var i = 0; i < 256; i++) {
-	document.getElementById("buttons").append("<button id='"+i+"' onclick='createAddress'>"+i+"</button>")
+	$(".container").append("<button id='"+i+"' onclick='createAddress'>"+i+"</button>")
 };
